017aaedda8335cb5534cffbb011396406d438518
BKDuncan <bailey.duncan@ucalgary.ca>
2017-11-29 20:24:23 -0700

    Update button listeners

--- app/src/main/java/com/example/bailey/dine_in_app/CreateFoodItem.java
+++ app/src/main/java/com/example/bailey/dine_in_app/CreateFoodItem.java
@@ -1,11 +1,13 @@
 package com.example.bailey.dine_in_app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class CreateFoodItem extends AppCompatActivity {
 
@@ -15,6 +17,17 @@ public class CreateFoodItem extends AppCompatActivity {
         setContentView(R.layout.activity_create_food_item);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        setButtonListener();
     }
 
+    private void setButtonListener(){
+        Button createFoodItem = (Button)this.findViewById(R.id.createFoodItemButton);
+        createFoodItem.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+//                Intent backToMain = new Intent(view.getContext(), MainActivity.class);
+//                startActivity(backToMain);
+            }
+        });
+    }
 }
--- app/src/main/java/com/example/bailey/dine_in_app/RestaurantHomeActivity.java
+++ app/src/main/java/com/example/bailey/dine_in_app/RestaurantHomeActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class RestaurantHomeActivity extends AppCompatActivity {
 
@@ -15,6 +16,45 @@ public class RestaurantHomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_restaurant_home);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        setButtonListeners();
+    }
+
+    private void setButtonListeners(){
+        Button navigateTransactions = (Button)this.findViewById(R.id.transactionButton);
+        navigateTransactions.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+//                Intent backToMain = new Intent(view.getContext(), MainActivity.class);
+//                startActivity(backToMain);
+            }
+        });
+
+        Button navigateFoodItems = (Button)this.findViewById(R.id.foodItemButton);
+        navigateFoodItems.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+//                Intent backToMain = new Intent(view.getContext(), MainActivity.class);
+//                startActivity(backToMain);
+            }
+        });
+
+        Button navigateOrders = (Button)this.findViewById(R.id.orderButton);
+        navigateOrders.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+//                Intent backToMain = new Intent(view.getContext(), MainActivity.class);
+//                startActivity(backToMain);
+            }
+        });
+
+        Button navigateTables = (Button)this.findViewById(R.id.tablesButton);
+        navigateTables.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+//                Intent backToMain = new Intent(view.getContext(), MainActivity.class);
+//                startActivity(backToMain);
+            }
+        });
     }
 
 }

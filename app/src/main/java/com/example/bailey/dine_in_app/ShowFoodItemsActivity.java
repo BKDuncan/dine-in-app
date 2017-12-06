b3c2968dfe5685b289d339f70b8389047623ad82
Anil_Sood <asood11@outlook.com>
2017-12-05 19:21:04 -0700

    Updating my local master

--- app/src/main/java/com/example/bailey/dine_in_app/ShowFoodItemsActivity.java
+++ app/src/main/java/com/example/bailey/dine_in_app/ShowFoodItemsActivity.java
@@ -1,11 +1,13 @@
 package com.example.bailey.dine_in_app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class ShowFoodItemsActivity extends AppCompatActivity {
 
@@ -15,7 +17,17 @@ public class ShowFoodItemsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_show_food_items);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
 
+    private void setButtonListeners() {
+        Button navigateTransactions = (Button) findViewById(R.id.createFoodItemButton);
+        navigateTransactions.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent createFoodItemActivity = new Intent(view.getContext(), CreateFoodItem.class);
+                startActivity(createFoodItemActivity);
+            }
+        });
     }
 
 }
--- app/src/main/java/com/example/bailey/dine_in_app/ShowOrderActivity.java
+++ app/src/main/java/com/example/bailey/dine_in_app/ShowOrderActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class ShowOrderActivity extends AppCompatActivity {
 
@@ -15,7 +16,18 @@ public class ShowOrderActivity extends AppCompatActivity {
         setContentView(R.layout.activity_show_order);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
 
+    private void setButtonListeners() {
+        Button showOrderDetail = (Button) this.findViewById(R.id.order_menu_button);
+        showOrderDetail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO: ShowOrderDetailActivity
+//                Intent showOrderDetailActivity = new Intent(view.getContext(), ShowOrderDetailActivity.class);
+//                startActivity(showOrderDetailActivity);
+            }
+        });
     }
 
 }
